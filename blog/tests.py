0a0105951105e07a1b09b4a77e491afeb28423d0
Huan Nguyen <huannguyen4404@gmail.com>
2019-10-16 00:25:37 +0700

    post list and detail

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
-
+from .models import Post
 # Create your tests here.
+class BlogTests(TestCase):
+    def setUp(self):
+        Post.objects.create(
+            title = 'my title',
+            body = 'test body'
+        )
+    
+    def test_string_representation(self):
+        post = Post(title='My entry title')
+        self.assertEqual(str(post), post.title)
+    
+    def test_post_list_view(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'my title')
+        self.assertTemplateUsed(response, 'blog/blog.html')
+
+    def test_post_detail_view(self):
+        response = self.client.get('/blog/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'my title')
+        self.assertTemplateUsed(response, 'blog/detail.html')
