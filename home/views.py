49ea47f255647ecae28a3919a015f2bd592aa48e
Nguyen The Huan <z.huan.nguyenthe@gree.net>
2019-10-15 17:00:56 +0700

    template

---
+++ home/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def index(request):
+    return render(request, 'pages/home.html')
+    # response = HttpResponse()
+    # response.writelines("<h1>Hi</h1>")
+    # response.write('Print from home app')
+    # return response
+def contact(request):
+    return render(request, 'pages/contact.html')
