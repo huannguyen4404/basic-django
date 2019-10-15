a864e194828840b398aeac6843ee7ed6418ba78e
Huan Nguyen <huannguyen4404@gmail.com>
2019-10-15 22:30:42 +0700

    freeze

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
-    title = models.Char
+# class Post(models.Model):
+#     title = models.Char
--- web_project/urls.py
+++ web_project/urls.py
@@ -1,4 +1,4 @@
-"""PythonWeb URL Configuration
+"""web_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'))
+    path('', include('home.urls')),
 ]
