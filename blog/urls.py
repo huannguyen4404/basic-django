fcfdafc3c73baafe0956b1106de6e043f2d85d7b
Huan Nguyen <huannguyen4404@gmail.com>
2019-10-16 23:37:23 +0700

    generic view

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list, name='post_list'),
-    path('<int:id>', views.detail, name='post_detail'),
+    # path('', views.list, name='post_list'),
+    path('', views.PostListView.as_view(), name='post_list'),
+    # path('<int:id>', views.detail, name='post_detail'),
+    path('<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 # Create your views here.
-def list(request):
-    Data = {'Posts': Post.objects.all().order_by("-date")}
-    return render(request, 'blog/blog.html', Data)
 
-def detail(request, id):
-    post = Post.objects.get(id=id)
-    return render(request, 'blog/detail.html', {'post': post})
+# def list(request):
+#     Data = {'Posts': Post.objects.all().order_by("-date")}
+#     return render(request, 'blog/blog.html', Data)
+class PostListView(ListView):
+    queryset = Post.objects.all().order_by('-date')
+    template_name = 'blog/blog.html'
+    context_object_name = 'Posts'
+    paginate_by = 10
+
+# def detail(request, id):
+#     post = Post.objects.get(id=id)
+#     return render(request, 'blog/detail.html', {'post': post})
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
