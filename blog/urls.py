2a509c63ca155669da3b71c87b77a964cc6db641
Nguyen The Huan <z.huan.nguyenthe@gree.net>
2019-10-16 11:03:03 +0700

    url name

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.list),
-    path('<int:id>', views.detail),
+    path('', views.list, name='post_list'),
+    path('<int:id>', views.detail, name='post_detail'),
 ]
